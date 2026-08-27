47d45c58b81015e2ce67eae9597525302763efbe
qylmtsm57-ui <qylmtsm57@gmail.com>
2026-08-27 12:04:58 +0300

    Refactor build.gradle.kts for dependencies and room
    Removed room schema configuration and adjusted dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,13 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+    id("androidx.room")
+    id("com.google.devtools.ksp")
+}
+
 android {
     namespace = "com.arttgr.alarm"
-
     compileSdk = 35
 
     defaultConfig {
@@ -24,13 +31,8 @@ android {
     }
 }
 
-room {
-    schemaDirectory("$projectDir/schemas")
-}
-
 dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2024.12.01")
-
     implementation(composeBom)
     androidTestImplementation(composeBom)
 
@@ -38,17 +40,15 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.10.0")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
-
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
-
     debugImplementation("androidx.compose.ui:ui-tooling")
 
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     ksp("androidx.room:room-compiler:2.6.1")
+}
 
     implementation("androidx.core:core:1.15.0")
-}
